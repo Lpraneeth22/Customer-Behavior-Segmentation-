e3337b3b6dcc2affa8f8e9e107c54355cf82c9ff
Lpraneeth22 <praneethl104@gmail.com>
2025-08-10 22:54:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 # =========================
 # 📂 Load Model & Scaler
 # =========================
-with open("model.pkl", "rb") as f:
+with open("Model.pkl", "rb") as f:
     model = pickle.load(f)
 
 with open("scal.pkl", "rb") as f:
@@ -81,3 +81,4 @@ if st.button("🔍 Predict Cluster"):
     # ---- Display Output ----
     st.success(f"Predicted Cluster: {cluster_num}")
     st.info(f"📌 Label: {cluster_labels.get(cluster_num, 'Unknown cluster')}")
+
